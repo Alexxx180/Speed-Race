cf097c879d4026d49e2209c2e8ebf56b5b73b4af
Студент ПТК <student@ptk.ru>
2021-11-15 14:24:06 +0300

    Alpha 1.02 | Smoothing view

---
+++ Assets/Scripts/TransferText.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class TransferText : MonoBehaviour
+{
+    public Text text;
+    public InputField input;
+
+    public void TransferDirect()
+    {
+        text.text = input.text;
+    }
+}
