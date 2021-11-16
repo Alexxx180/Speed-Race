0dc44740625eb71092913daef2d40afee6d6a407
Студент ПТК <student@ptk.ru>
2021-11-16 14:07:08 +0300

    Alpha 1.03 | Movement started

---
+++ Assets/Scripts/Barrier.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+
+public class Barrier : MonoBehaviour
+{
+    PlayerStats player;
+
+    private void Awake()
+    {
+        player = GameObject.FindGameObjectWithTag("Player");
+    }
+
+    public void OnTriggerEnter2D(collision2D collision)
+    {
+        if (collision.CompareTag("Player"))
+        {
+            player.Hurt();
+        }
+    }
+}
---
+++ Assets/Scripts/PlayerStats.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+
+public class PlayerStats : MonoBehaviour
+{
+    public int maxHp = 1;
+    [Range(0..maxHp)] public int hp = 1;
+
+    public GameObject menu;
+
+    public float runSpeed = 40f;
+    float horizontalMove = 0f;
+
+    public int leftBorder = -250;
+    public int rightBorder = 250;
+
+    private void Hurt()
+    {
+        hp--;
+        if (hp <= 0)
+        {
+            menu.SetActive(true);
+        }
+    }
+
+    void Update()
+    {
+        horizontalMove = Input.GetAxisRaw("Horizontal")) * runSpeed;
+        gameObject.transform.position = new Vector3(Mathf.Clamp(gameObject.transform.position.x * horizontalMove, leftBorder, rightBorder), 0, 0);
+    }
+}
