3ce24cdfb440e7e89acb6437a8ea6a3e4e493cb9
Alexxx180 <alexx120702@gmail.com>
2021-11-22 14:29:37 +0300

    Alpha 1.07 | Small improvement Prefab Logic

--- Assets/Scripts/RandomAppearing.cs
+++ Assets/Scripts/RandomAppearing.cs
@@ -6,10 +6,10 @@ using UnityEngine;
 public class RandomAppearing : MonoBehaviour
 {
     public List<GameObject> obstacles;
+    public List<GameObject> appearZones;
 
-
-    public float minTime = 2f;
-    public float maxTime = 10f;
+    public float minTime = 2000f;
+    public float maxTime = 10000f;
 
     public float toTime = 10f;
     public float time = 0f;
@@ -19,8 +19,13 @@ public class RandomAppearing : MonoBehaviour
     {
         if (time >= toTime)
         {
-            GameObject obstacle = Instantiate(obstacles[Random.Range(0, 6)]);
+            int i = Random.Range(0, 2);
+            int pos = Random.Range(0, 3);
+
+            GameObject obstacle = Instantiate(obstacles[i]);
             obstacle.transform.SetParent(gameObject.transform);
+            obstacle.transform.position = appearZones[pos].transform.position;
+
             toTime = Random.Range(minTime, maxTime);
             time = 0f;
         }
