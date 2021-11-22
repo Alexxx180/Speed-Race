7e13a8bf239c44c62ccb84b94d4051e5d3606cb7
Alexxx180 <alexx120702@gmail.com>
2021-11-22 13:45:17 +0300

    Alpha 1.05 | Few Barriers

--- Assets/Scripts/BarierMoves.cs
+++ Assets/Scripts/BarierMoves.cs
@@ -2,21 +2,34 @@
 
 public class BarierMoves : MonoBehaviour
 {
-    public float speed = 4f;
-    //public Rigidbody body;
+    public float speed = 0.5f;
+    public float sizeInc = 1f;
+    public float moveInc = 2.464285714285714f;
+    public int rotateX = 0;
+
+    private Vector3 minPosition = new Vector3(-35f, 60f, 0f);
+    private Vector3 minSize = new Vector3(0.1f, 0.1f, 0.1f);
+
+    private Vector3 maxPosition = new Vector3(-380f, -385f, 0f);
+    private Vector3 maxSize = new Vector3(1.5f, 1.5f, 1.5f);
+
+    public void Awake()
+    {
+        sizeInc *= speed;
+        moveInc *= speed;
+    }
+
     public void Update()
     {
-        
-        //Vector3 targetVelocity = new Vector2(horizontalMove * 10f, body.velocity.y);
-        // And then smoothing it out and applying it to the character
-        //body.velocity = Vector3.SmoothDamp(body.velocity, targetVelocity, ref _Velocity, 0.05f);
-
-
         Vector3 mem = gameObject.transform.position;
-        float z = mem.z - 4.385964912280702f * Time.deltaTime;
-        float y = mem.y - 1 * Time.deltaTime;
-        gameObject.transform.position = new Vector3(mem.x, y, Mathf.Clamp(z, 0, 1500));
-        if (y <= -200f)
+        Vector3 size = gameObject.transform.localScale;
+        
+        float x = mem.x - (moveInc * rotateX) * Time.deltaTime;
+        float y = mem.y - moveInc * 1.115942028985507f * Time.deltaTime;
+        float inc = size.x + sizeInc * Time.deltaTime;
+        gameObject.transform.position = new Vector3(x, y, 0);
+        gameObject.transform.localScale = new Vector3(inc, inc, inc);
+        if (inc >= maxSize.x)
             Destroy(gameObject);
     }
 }
